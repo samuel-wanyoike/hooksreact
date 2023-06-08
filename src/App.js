4af7c87579da68ccaaa78e4a4da462b36075c16b
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-06-08 23:57:52 +0300

    created refs use cases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-  
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth)
-  }
-  
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
+  const [name, setName] = useState('');
 
-    //removes the event listener when the useeffect is removed
-    return window.removeEventListener('resize', handleResize)
-  }, [])
+ //useRef does not cause a rerender of the component when state is updated. it is not part of the component lifecycle
+ const renderCount = useRef(0);
+
+ useEffect(() => {
+  renderCount.current = renderCount.current+1
+  });
+
+//use useRef to reference a HTML element
+  const inputRef = useRef();
+  const focus =() => {
+    inputRef.current.focus();
+  }
+
+  //using a ref to store prevous value of a state
+  const prevNameRef = useRef('');
+  useEffect(() => {
+    prevNameRef.current = name
+  }, [name]);
 
   return (
-    <div className="App">
-
-      {windowWidth}
+    <div>
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+      <div>my current name is: {name} but it used to be {prevNameRef.current}</div>
+      <div>my render count is: {renderCount.current}</div>
+      <button onClick={focus}>Focus</button>
     </div>
   );
 }
