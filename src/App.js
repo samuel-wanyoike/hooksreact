443ceec6ed75a67986aa64eefe68f8f870322774
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-06-11 02:03:24 +0300

    finalized todo list project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { useReducer, useState } from 'react';
 import './App.css';
+import { Todos } from './Todos';
 
+//GLOBAL VARIABLE
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  COMPLETE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+};
 
-const ACTIONS = {
-  ADD_TODO: 'add-todo'
-}
+//REDUCER FUNCTION
+function reducer(todos, action) {
+  switch(action.type){
+
+    case ACTIONS.ADD_TODO:
+      return [...todos, newToDo(action.payload.name)];
+
+    case ACTIONS.COMPLETE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {...todo, complete: !todo.complete}
+        }
+        return todo;
+      } );
+    
+    case ACTIONS.DELETE_TODO:
+        return todos.filter(todo => todo.id !== action.payload.id);
+
+    default:
+      return todos;
+        
+  }
+};
+
+//defines the todo list objects
+const newToDo = (name) => {
+  return {id: Date.now(), name: name, complete: false}
+};
+
+//MAIN FUNCTION
 function App() {
-  const [todos, dispatch] = useReducer(reducer, { count: 0 });
+  const [todos, dispatch] = useReducer(reducer, []);
   const [name, setName] =useState('');
 
-  const handleSubmit = () => {
-    dispatch({type: ACTIONS.ADD_TODO})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({type: ACTIONS.ADD_TODO, payload: {name: name}});
+    setName('')
   }
 
   return (
@@ -18,6 +55,9 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input type='text' value={name} onChange={e => setName(e.target.value)}/>
       </form>
+      {todos.map((todo) => {
+        return <Todos key={todo.id} todo={todo} dispatch={dispatch}/>
+      })}
     </div>
   );
 }
